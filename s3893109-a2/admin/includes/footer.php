fcc0724019807566e9e73a8c8b66db8aa6ee16f0
Anita Afrin Arpita <85857180+ANITA280@users.noreply.github.com>
2024-08-03 15:53:21 +1000

    Add files via upload

---
+++ s3893109-a2/admin/includes/footer.php
@@ -0,0 +1,43 @@
+<div class="item5">
+
+    <div class="boxes">
+      <div class=logoFoot><span id="namefoot">LIFE</span></div>
+    <div id="box1">
+      
+      <a href="index.html">Home</a>
+      <a href="services.html">Services</a>
+      <a href="contactusForm.html">Contact Us</a>
+     
+    </div>
+    <div id="box4">
+      <a href="aboutus.html">About Us</a>
+      <a href="privacy.html">Privacy Policy</a>
+      <a href="sitemap.html">Sitemap</a>
+    </div>
+
+    <div id="box2">
+
+  <!--Links of Covid 19 resources -->
+   <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019">WHO Covid-19 Resources</a>
+   
+     <!--Links of Covid 19 resources based on states-->
+<a href="https://app.powerbi.com/view?r=eyJrIjoiZTY4NTI1NzQtYTBhYy00ZTY4LTk3NmQtYjBjNzdiOGMzZjM3IiwidCI6ImI0NmMxOTA4LTAzMzQtNDIzNi1iOTc4LTU4NWVlODhlNDE5OSJ9">Australian Capital Territory Resources</a></li>
+<a href="https://www.nsw.gov.au/covid-19/find-the-facts-about-covid-19">New South Wales Resources</a>
+<a href="https://coronavirus.nt.gov.au/current-status">Northern Territory Resources</a>
+<a href="https://www.qld.gov.au/health/conditions/health-alerts/coronavirus-covid-19/current-status/statistics">Queensland Resources</a>
+
+    
+     
+    
+  </div>
+  <div id="box3">
+   
+    <a href="https://www.covid-19.sa.gov.au/home/dashboard">South Australia Covid-19 Resources</a>
+    <a href="https://www.coronavirus.tas.gov.au/facts/tasmanian-statistics">Tasmania Covid-19 Resources</a>
+    <a href="https://www.coronavirus.vic.gov.au/victorian-coronavirus-covid-19-data">Victoria Covid-19 Resources</a>
+    <a href="https://experience.arcgis.com/experience/359bca83a1264e3fb8d3b6f0a028d768">Western Australia Covid-19 Resources</a>
+  </div>
+</div>
+<div style="clear: both;"></div>
+<div style="margin-top: 220px; margin-right: 800px; font-size: small;">Copyright &copy 2021 LIFE. All rights reserved</div>
+</div>
